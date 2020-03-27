66af9e252505b13a58ab2ac22d97019a1ec4e83f
scott <scott.stanie@gmail.com>
2020-03-27 15:33:56 -0500

    new script for avg phase vs elevation

--- helpers/avg_igrams.py
+++ helpers/avg_igrams.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 """
-Script for making small tifs of complex data for quick looks
-
-Uses gdal_translate to reduce size, then gdal_calc to get power from complex values
+Averages all unwrapped igrams, making images of the averge phase per date
 """
 import itertools
+import subprocess
 # import multiprocessing
 # import os
 import numpy as np
@@ -31,6 +30,7 @@ if __name__ == "__main__":
     # gdal_translate -outsize 2% 2% S1B_20190325.geo.vrt looked_S1B_20190325.geo.tif
     ifg_date_list = sario.find_igrams(".")
     unw_file_list = [f.replace(".int", ".unw") for f in sario.find_igrams(".", parse=False)]
+    # unw_file_list = [f.replace(".int", ".unwflat") for f in sario.find_igrams(".", parse=False)]
 
     geo_dates = sorted(set(itertools.chain.from_iterable(ifg_date_list)))
     with rio.open(unw_file_list[0]) as ds:
@@ -38,9 +38,7 @@ if __name__ == "__main__":
         transform = ds.transform
         crs = ds.crs
 
-    print(out.shape)
-
-    for gdate in geo_dates:
+    for (idx, gdate) in enumerate(geo_dates):
         cur_unws = [f for (pair, f) in zip(ifg_date_list, unw_file_list) if gdate in pair]
         out = 0
         for unwf in cur_unws:
@@ -48,7 +46,8 @@ if __name__ == "__main__":
                 # phase band is #2, amplitde is 1
                 out += ds.read(2)
 
-        print("Averaging {} unwrapped igrams for {}".format(len(cur_unws), gdate))
+        print("Averaging {} unwrapped igrams for {} ({} out of {})".format(
+            len(cur_unws), gdate, idx + 1, len(geo_dates)))
         out /= len(cur_unws)
         outfile = "avg_" + gdate.strftime("%Y%m%d") + ".tif"
         with rio.open(
@@ -60,6 +59,23 @@ if __name__ == "__main__":
                 height=out.shape[0],
                 width=out.shape[1],
                 count=1,
+                nodata=0,
                 dtype=out.dtype,
         ) as dst:
             dst.write(out, 1)
+        # And make it easily viewable as a Byte image
+        subprocess.call([
+            "gdal_translate",
+            "-quiet",
+            "-scale",
+            "-co",
+            "TILED=YES",
+            "-co",
+            "COMPRESS=LZW",
+            "-ot",
+            "Byte",
+            outfile,
+            outfile.replace(".tif", "_byte.tif"),
+        ])
+        # if idx > 3:
+        #     break
---
+++ helpers/elevation_vs_phase.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+"""
+Makes scatterplots of the phase values vs elevation to see if linear trend exists
+"""
+import matplotlib.pyplot as plt
+# import multiprocessing
+# import os
+import numpy as np
+
+from apertools import sario
+
+
+def save_unw_vs_elevation(unw_file_list, every=50):
+    points = np.empty((0, 2))
+    elpts = sario.load("elevation_looked.dem")
+    mask = ~np.isnan(sario.load(unw_file_list[0].replace(".unw", ".unwflat")))
+    # elpts = elpts[:, :400]
+    elpts = elpts[mask]
+    elpts = elpts.reshape((-1, ))
+
+    for f in unw_file_list[::every]:
+        out = sario.load(f)
+        out -= np.mean(out)
+        # new_pts = np.vstack((elpts, out[:, :400][mask].reshape((-1, )))).T
+        new_pts = np.vstack((elpts, out[mask].reshape((-1, )))).T
+        points = np.vstack((points, new_pts))
+    np.save("elevation_points.npy", points)
+
+
+def plot_unw_vs_elevation():
+    elpts = np.load("elevation_points.npy")[::1000]
+    plt.figure()
+    plt.scatter(elpts[:, 0], elpts[:, 1], s=.1)
+    plt.xlabel("elevation")
+    plt.ylabel("unwrapped phase (rad)")
+    plt.title("sample of all unwrapped igram points vs elevation")
+    plt.show(block=True)
+
+
+if __name__ == "__main__":
+    # gdal_translate -outsize 2% 2% S1B_20190325.geo.vrt looked_S1B_20190325.geo.tif
+    ifg_date_list = sario.find_igrams(".")
+    unw_file_list = [f.replace(".int", ".unw") for f in sario.find_igrams(".", parse=False)]
+    # unw_file_list = [f.replace(".int", ".unwflat") for f in sario.find_igrams(".", parse=False)]
+    save_unw_vs_elevation(unw_file_list)
+    plot_unw_vs_elevation()
