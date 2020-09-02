570624333518c9b453c2d5f231572db7073e95f7
scott <scott.stanie@gmail.com>
2020-09-02 18:40:54 -0500

    fix cmds

--- apertools/utils.py
+++ apertools/utils.py
@@ -479,19 +479,21 @@ def get_cache_dir(force_posix=False, app_name="apertools"):
 
 
 def az_inc_to_enu(infile, outfile='geo_los_enu.tif'):
-    cmd = f'gdal_calc.py -A {infile} -B {infile} --A_band=1 --B_band=2 --outfile geo_los_e.tif --calc="sin(deg2rad(A)) * cos(deg2rad(B+90))" '
+    cmd = f'gdal_calc.py --quiet -A {infile} -B {infile} --A_band=1 --B_band=2 --outfile tmp_los_east.tif --calc="sin(deg2rad(A)) * cos(deg2rad(B+90))" '
     print(cmd)
     subprocess.run(cmd, check=True, shell=True)
 
-    f'gdal_calc.py -A {infile} -B {infile} --A_band=1 --B_band=2 --outfile geo_los_n.tif --calc="sin(deg2rad(A)) * sin(deg2rad(B+90))" '
+    cmd = f'gdal_calc.py --quiet -A {infile} -B {infile} --A_band=1 --B_band=2 --outfile tmp_los_north.tif --calc="sin(deg2rad(A)) * sin(deg2rad(B+90))" '
     print(cmd)
     subprocess.run(cmd, check=True, shell=True)
 
-    f'gdal_calc.py -A {infile} -B {infile} --A_band=1 --B_band=2 --outfile geo_los_u.tif --calc="cos(deg2rad(A))" '
+    cmd = f'gdal_calc.py --quiet -A {infile} -B {infile} --A_band=1 --B_band=2 --outfile tmp_los_up.tif --calc="cos(deg2rad(A))" '
     print(cmd)
     subprocess.run(cmd, check=True, shell=True)
 
-    f'gdal_merge.py -separate -o {outfile} geo_los_e.tif geo_los_n.tif geo_los_u.tif '
+    cmd = f'gdal_merge.py -separate -o {outfile} tmp_los_east.tif tmp_los_north.tif tmp_los_up.tif '
     print(cmd)
     subprocess.run(cmd, check=True, shell=True)
-    subprocess.run('rm -f geo_los_e.tif geo_los_e.tif geo_los_e.tif', shell=True, check=True)
+    subprocess.run('rm -f tmp_los_east.tif tmp_los_north.tif tmp_los_up.tif',
+                   shell=True,
+                   check=True)
