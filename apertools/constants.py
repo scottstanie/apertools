a3a1d0696bef2e513cc36a3eb7ef885023dfc13c
scott <scott.stanie@gmail.com>
2021-08-13 14:17:09 -0500

    ad uavsar wvl

--- apertools/constants.py
+++ apertools/constants.py
@@ -3,3 +3,7 @@ from numpy import pi
 SENTINEL_WAVELENGTH = 5.5465763  # cm
 PHASE_TO_CM = SENTINEL_WAVELENGTH / (4 * pi)
 P2MM = PHASE_TO_CM * 10 * 365  # (cm / day) -> (mm / yr)
+
+UAVSAR_WAVELENGTH = 23.8403545  # cm
+PHASE_TO_CM_UA = UAVSAR_WAVELENGTH / (4 * pi)
+P2MM_UA = PHASE_TO_CM_UA * 10 * 365
