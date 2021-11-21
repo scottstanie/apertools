9487ddd9fbb36dbbefe720985916ba22f7a39ea7
scott <scott.stanie@gmail.com>
2021-11-21 15:44:57 -0600

    add another scale bar option

--- apertools/plotting.py
+++ apertools/plotting.py
@@ -1060,3 +1060,78 @@ def _point_along_line(ax, start, distance, angle=0, tol=0.01):
     end = _upper_bound(start, direction, distance, dist_func)
 
     return _distance_along_line(start, end, distance, dist_func, tol)
+
+
+def scale_bar(
+    ax,
+    proj,
+    length,
+    location=(0.5, 0.05),
+    linewidth=3,
+    units="km",
+    m_per_unit=1000,
+    zorder=1,
+):
+    """
+    http://stackoverflow.com/a/35705477/1072212
+    ax is the axes to draw the scalebar on.
+    proj is the projection the axes are in
+    location is center of the scalebar in axis coordinates ie. 0.5 is the middle of the plot
+    length is the length of the scalebar in km.
+    linewidth is the thickness of the scalebar.
+    units is the name of the unit
+    m_per_unit is the number of meters in a unit
+    """
+    import cartopy.crs as ccrs
+    from matplotlib import patheffects
+
+    # find lat/lon center to find best UTM zone
+    x0, x1, y0, y1 = ax.get_extent(proj.as_geodetic())
+    # Projection in metres
+    utm = ccrs.UTM(latlon.utm_from_lon((x0 + x1) / 2))
+    # Get the extent of the plotted area in coordinates in metres
+    x0, x1, y0, y1 = ax.get_extent(utm)
+    print(x0, x1, y0, y1)
+    # Turn the specified scalebar location into coordinates in metres
+    sbcx, sbcy = x0 + (x1 - x0) * location[0], y0 + (y1 - y0) * location[1]
+    # Generate the x coordinate for the ends of the scalebar
+    bar_xs = [sbcx - length * m_per_unit / 2, sbcx + length * m_per_unit / 2]
+    # buffer for scalebar
+    buffer = [patheffects.withStroke(linewidth=5, foreground="w")]
+    # Plot the scalebar with buffer
+    ax.plot(
+        bar_xs,
+        [sbcy, sbcy],
+        transform=utm,
+        color="k",
+        linewidth=linewidth,
+        path_effects=buffer,
+        zorder=zorder,
+    )
+    # buffer for text
+    buffer = [patheffects.withStroke(linewidth=3, foreground="w")]
+    # Plot the scalebar label
+    t0 = ax.text(
+        sbcx,
+        sbcy,
+        str(length) + " " + units,
+        transform=utm,
+        horizontalalignment="center",
+        verticalalignment="bottom",
+        path_effects=buffer,
+        zorder=zorder + 1,
+    )
+    # left = x0 + (x1 - x0) * 0.05
+    # Plot the N arrow
+    #     t1 = ax.text(left, sbcy, u'\u25B2\nN', transform=utm,
+    #         horizontalalignment='center', verticalalignment='bottom',
+    #         path_effects=buffer, zorder=2)
+    # Plot the scalebar without buffer, in case covered by text buffer
+    ax.plot(
+        bar_xs,
+        [sbcy, sbcy],
+        transform=utm,
+        color="k",
+        linewidth=linewidth,
+        zorder=zorder + 2,
+    )
