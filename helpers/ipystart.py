c9f08a24342404a0a2009064e39195c77c255ab9
scott <scott.stanie@gmail.com>
2021-11-29 14:41:25 -0600

    reorder imports for screen

--- helpers/ipystart.py
+++ helpers/ipystart.py
@@ -1,5 +1,22 @@
+def in_screen():
+    """Checks if we are currently running in a screen
+    If so, just use agg backend for matplotlib
+    (suggestion from https://stackoverflow.com/a/3054314)
+    # Answer from: https://stackoverflow.com/a/5392681
+    """
+    return os.environ.get("STY") is not None
+
+
 get_ipython().run_line_magic("load_ext", "autoreload")
 get_ipython().run_line_magic("autoreload", "2")
+
+# Need to do this BEFORE pyplot
+import matplotlib
+
+if in_screen():
+    print("Currently in screen, using agg backend")
+    matplotlib.use("agg")
+
 import os
 from collections import Counter
 import sys
@@ -10,27 +27,15 @@ import itertools
 import subprocess
 
 from tqdm import tqdm
-import matplotlib
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+
 # import hdf5plugin
 import h5py
 import rasterio as rio
 import xarray as xr
-
-def in_screen():
-    """Checks if we are currently running in a screen
-    If so, just use agg backend for matplotlib
-    (suggestion from https://stackoverflow.com/a/3054314)
-    # Answer from: https://stackoverflow.com/a/5392681
-    """
-    return os.environ.get("STY") is not None
-
-
-if in_screen():
-    print("Currently in screen, using agg backend")
-    matplotlib.use("agg")
 
 plt.ion()
 
@@ -41,6 +46,7 @@ import apertools.sario as sario
 import apertools.plotting as plotting
 import apertools.utils as utils
 import apertools.parsers as parsers
+
 # import apertools.gps as gps
 # import apertools.latlon as latlon
 # from apertools.scripts import *
