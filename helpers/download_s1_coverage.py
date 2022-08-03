34392565b8eaf3d4f6046b66b1df73a300896c2d
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2022-08-03 08:06:58 -0700

    touch up coverage download

--- helpers/download_s1_coverage.py
+++ helpers/download_s1_coverage.py
@@ -1,22 +1,33 @@
 #!/usr/bin/env python
-from logging import getLogger, StreamHandler, DEBUG
-from asyncio.log import logger
 import sys
 
 import pandas as pd
 
-from apertools import asfdownload
+from apertools import asfdownload, log
 
-logger = getLogger(__name__)
 
 def main():
-    wkt_file = sys.argv[1]
+    logger = log.get_log()
+    try:
+        wkt_file = sys.argv[1]
+    except IndexError:
+        logger.error("No WKT file provided")
+        raise
+    try:
+        out_dir = sys.argv[2]
+    except IndexError:
+        out_dir = "."
+    logger.info(f"Downloading S1 metadata from WKT file {wkt_file} into {out_dir}")
+
     today = str(pd.Timestamp.today().date())
-    biweekly_dates =  pd.date_range(start="2014-01-01", end=today, freq='2W').strftime("%Y-%m-%d")
+    biweekly_dates = pd.date_range(start="2014-09-01", end=today, freq="2W").strftime(
+        "%Y-%m-%d"
+    )
 
     for start, end in zip(biweekly_dates[:-1], biweekly_dates[1:]):
         logger.info(f"Downloading {start} to {end}")
-        asfdownload.form_url(
+        asfdownload.download_data(
+            out_dir=".",
             wkt_file=wkt_file,
             start=start,
             end=end,
