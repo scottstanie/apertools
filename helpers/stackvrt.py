9466510c037562d52f990e3a632f643411a7466b
Scott Staniewicz <scott.j.staniewicz@jpl.nasa.gov>
2022-08-03 08:07:28 -0700

    get subset of vrt

--- helpers/stackvrt.py
+++ helpers/stackvrt.py
@@ -32,6 +32,22 @@ def get_cli_args():
         default="slcs_base.vrt",
         help="Name of output SLC containing all images (defaul = %(default)s)",
     )
+    parser.add_argument(
+        "--subset-bbox",
+        type=int,
+        nargs=4,
+        metavar=("left", "bottom", "right", "top"),
+        default=None,
+        help="Bounding box to subset the stack to (default is full stack)",
+    )
+    parser.add_argument(
+        "--target-extent",
+        type=int,
+        nargs=4,
+        metavar=("xmin", "ymin", "xmax", "ymax"),
+        default=None,
+        help="Target extent (like GDAL's `-te` option), alternative to subset the stack.",
+    )
     args = parser.parse_args()
     return args
 
@@ -85,25 +101,32 @@ def create_vrt_stack(file_list, outfile="slcs_base.vrt"):
     out_raster = None  # Force write
 
 
-def create_vrt_stack_manual(file_list, outfile="slcs_base.vrt"):
+def create_vrt_stack_manual(
+    file_list, subset_bbox=None, target_extent=None, outfile="slcs_base.vrt", use_abs_path=True,
+):
     # Use the first file in the stack to get size, transform info
     ds = gdal.Open(file_list[0])
     xsize = ds.RasterXSize
     ysize = ds.RasterYSize
     ds = None
     print("write vrt file for stack directory")
+    xoff, yoff, xsize_sub, ysize_sub = get_subset_bbox(
+        xsize, ysize, subset_bbox=subset_bbox, target_extent=target_extent
+    )
     with open(outfile, "w") as fid:
-        fid.write(f'<VRTDataset rasterXSize="{xsize}" rasterYSize="{ysize}">\n')
+        fid.write(f'<VRTDataset rasterXSize="{xsize_sub}" rasterYSize="{ysize_sub}">\n')
 
         for idx, filename in enumerate(file_list, start=1):
+            if use_abs_path:
+                filename = os.path.abspath(filename)
             date = _get_date(filename)
             outstr = f"""    <VRTRasterBand dataType="CFloat32" band="{idx}">
         <SimpleSource>
             <SourceFilename>{filename}</SourceFilename>
             <SourceBand>1</SourceBand>
             <SourceProperties RasterXSize="{xsize}" RasterYSize="{ysize}" DataType="CFloat32"/>
-            <SrcRect xOff="0" yOff="0" xSize="{xsize}" ySize="{ysize}"/>
-            <DstRect xOff="0" yOff="0" xSize="{xsize}" ySize="{ysize}"/>
+            <SrcRect xOff="{xoff}" yOff="{yoff}" xSize="{xsize_sub}" ySize="{ysize_sub}"/>
+            <DstRect xOff="0" yOff="0" xSize="{xsize_sub}" ySize="{ysize_sub}"/>
         </SimpleSource>
         <Metadata domain="slc">
             <MDI key="Date">{date}</MDI>
@@ -114,6 +137,42 @@ def create_vrt_stack_manual(file_list, outfile="slcs_base.vrt"):
             fid.write(outstr)
 
         fid.write("</VRTDataset>")
+
+
+def get_subset_bbox(xsize, ysize, subset_bbox=None, target_extent=None):
+    """Get the subset bounding box for a given target extent.
+
+    Parameters
+    ----------
+    xsize : int
+        size of the x dimension of the image
+    ysize : int
+        size of the y dimension of the image
+    subset_bbox : tuple[int], optional
+        Desired bounding box of subset as (left, bottom, right, top)
+    target_extent : tuple[int], optional
+        Desired bounding box in the `-te` gdal format, (xmin, ymin, xmax, ymax)
+
+    Returns
+    -------
+    xoff, yoff, xsize_sub, ysize_sub : tuple[int]
+    """
+
+    if subset_bbox is not None:
+        left, bottom, right, top = subset_bbox
+        xoff = left
+        yoff = top
+        xsize_sub = right - left
+        ysize_sub = bottom - top
+    elif target_extent is not None:
+        # -te xmin ymin xmax ymax
+        xoff, yoff, xmax, ymax = target_extent
+        xsize_sub = xmax - xoff
+        ysize_sub = ymax - yoff
+    else:
+        xoff, yoff, xsize_sub, ysize_sub = 0, 0, xsize, ysize
+
+    return xoff, yoff, xsize_sub, ysize_sub
 
 
 def _get_date(filename):
@@ -143,4 +202,9 @@ if __name__ == "__main__":
     utils.mkdir_p(args.out_dir)
     outfile = os.path.join(args.out_dir, args.out_vrt_name)
     # create_vrt_stack(file_list, outfile=outfile)
-    create_vrt_stack_manual(file_list, outfile=outfile)
+    create_vrt_stack_manual(
+        file_list,
+        outfile=outfile,
+        subset_bbox=args.subset_bbox,
+        target_extent=args.target_extent,
+    )
